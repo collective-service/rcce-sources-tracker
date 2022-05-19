80fdf7f857c23bfb821e15da2402613cee594675
amadu <ndongamadou@gmail.com>
2022-05-19 14:13:08 +0100

    fix geojson

--- js/bundle.js
+++ js/bundle.js
@@ -745,7 +745,7 @@ function drawPanelChart(name){
     $('#globalStats').addClass('hidden');
     $('#overview').removeClass('hidden');
 }//drawPanelChart
-let geodataUrl = 'data/wld.json';
+let geodataUrl = 'data/wld052022.json';
 let data_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQwVG8xzJogJgeBzk5-mLaA7BOGWhwU_Z6iGrnGQwPT2OAInzFYX-5hNYh2aFyqn0sVh0PpFikSJuEq/pub?gid=1612863274&single=true&output=csv';
 // 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRQDTIju76FYADX9ZKbKBD1JBA7eFLv86Y8ltOTs24eLqrf3FnKJENmtcUkP1HUMCQq7JL9hgwofz0q/pub?gid=1612863274&single=true&output=csv';
 
@@ -759,7 +759,7 @@ $( document ).ready(function(){
             d3.json(geodataUrl),
             d3.csv(data_url),
         ]).then(function(data){
-            geomData = topojson.feature(data[0], data[0].objects.worldtopo12022020);
+            geomData = topojson.feature(data[0], data[0].objects.geom);
             sourcesData = data[1];
 
             sourcesDataFiltered = data[1];
